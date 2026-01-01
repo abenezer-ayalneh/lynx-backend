0bfeceafd514d11766f556ee83ab8221d04dd911
abenezer-ayalneh <abenezer.ayalneh.42@gmail.com>
2026-01-01 08:36:19 +0300

    feat: add `ToUpperCase` transform to CreateWordDto and update seed data formatting

--- src/admin/words/dto/create-word.dto.ts
+++ src/admin/words/dto/create-word.dto.ts
@@ -1,26 +1,35 @@
+import { Transform } from 'class-transformer'
 import { IsBoolean, IsNotEmpty, IsOptional, IsString } from 'class-validator'
 
+const ToUpperCase = () => Transform(({ value }) => (typeof value === 'string' ? value.toUpperCase() : (value as unknown)))
+
 export default class CreateWordDto {
+	@ToUpperCase()
 	@IsString()
 	@IsNotEmpty()
 	key: string
 
+	@ToUpperCase()
 	@IsString()
 	@IsNotEmpty()
 	cue_word_1: string
 
+	@ToUpperCase()
 	@IsString()
 	@IsNotEmpty()
 	cue_word_2: string
 
+	@ToUpperCase()
 	@IsString()
 	@IsNotEmpty()
 	cue_word_3: string
 
+	@ToUpperCase()
 	@IsString()
 	@IsNotEmpty()
 	cue_word_4: string
 
+	@ToUpperCase()
 	@IsString()
 	@IsNotEmpty()
 	cue_word_5: string
